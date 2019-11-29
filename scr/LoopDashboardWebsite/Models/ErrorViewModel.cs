d95a5e7703de98b7142229c5a592f9e7b751bf6a
Christian F. Aagaard <cfa@innovait.dk>
2019-11-29 09:29:22 +0100

    Added

---
+++ scr/LoopDashboardWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoopDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
